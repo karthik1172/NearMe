21cd6dd452986e0c9292b2469bea92b7952f8136
karthik1172 <karthik.p.rashinkar@gmail.com>
2024-09-18 19:59:13 +0530

    Added Map and Search TextFiel

--- NearMe/ContentView.swift
+++ NearMe/ContentView.swift
@@ -6,16 +6,30 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    @State private var query: String = ""
+    @State private var selectedDetent: PresentationDetent = .fraction(0.15)
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Map()
+                .sheet(isPresented: .constant(true), content: {
+                    VStack {
+                        TextField("Search", text: $query)
+                            .textFieldStyle(.roundedBorder)
+                            .padding()
+                            .onSubmit {
+                                // Core fired when you returned in textfield.
+                            }
+                        Spacer()
+                    }
+                    .presentationDetents([.fraction(0.15), .medium, .large], selection: $selectedDetent)
+                    .presentationDragIndicator(.visible)
+                    .interactiveDismissDisabled()
+                    .presentationBackgroundInteraction(.enabled(upThrough: .medium))
+                })
         }
-        .padding()
     }
 }
 

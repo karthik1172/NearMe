7807be24bdd13101616d4ca5fdcf0972ced436ed
karthik1172 <karthik.p.rashinkar@gmail.com>
2024-09-18 19:36:57 +0530

    Initial Commit

---
+++ NearMe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NearMe
+//
+//  Created by Karthik Rashinkar on 18/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NearMe/NearMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NearMeApp.swift
+//  NearMe
+//
+//  Created by Karthik Rashinkar on 18/09/24.
+//
+
+import SwiftUI
+
+@main
+struct NearMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
